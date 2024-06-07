5335cbf151c4e84f76e5c44e2ba2d8f20cb4752d
Le Tho <letho11112002@gmail.com>
2024-06-07 10:10:51 +0700

    connect to mongoDb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => console.log("MongoDb connect success"))
+    .catch((error) => {
+        console.log("mongoDb connect error: ", error);
+    });
+
 const app = express();
 
 app.listen(3000, () => {
