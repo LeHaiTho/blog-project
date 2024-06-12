237d24f0062d70e8827a2b82fa1691aecebf265c
LeHaiTho <letho11112002@gmail.com>
2024-06-12 12:35:56 +0700

    add functionality to the sign up

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,29 +2,29 @@ import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 export const signup = async (req, res, next) => {
-    const { username, email, password } = req.body;
-    if (
-        !username ||
-        !password ||
-        !email ||
-        password === "" ||
-        email === "" ||
-        username === ""
-    ) {
-        next(errorHandler(400, "All fields required"));
-    }
+  const { username, email, password } = req.body;
+  if (
+    !username ||
+    !password ||
+    !email ||
+    password === "" ||
+    email === "" ||
+    username === ""
+  ) {
+    next(errorHandler(400, "All fields required"));
+  }
 
-    const hashedpassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({
-        username,
-        email,
-        password: hashedpassword,
-    });
+  const hashedpassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({
+    username,
+    email,
+    password: hashedpassword,
+  });
 
-    try {
-        await newUser.save();
-        res.json({ message: "new user was created successfully" });
-    } catch (error) {
-        next(error);
-    }
+  try {
+    await newUser.save();
+    res.json("Sign up successful");
+  } catch (error) {
+    next(error);
+  }
 };
