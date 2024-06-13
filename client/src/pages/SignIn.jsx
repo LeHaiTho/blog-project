3557efb9a278fe0f47c7be961021f65bbe4841c5
LeHaiTho <letho11112002@gmail.com>
2024-06-13 15:11:55 +0700

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,12 +2,19 @@ import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { HiInformationCircle } from "react-icons/hi";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  signInStart,
+  signInFailure,
+  signInSuccess,
+} from "../redux/user/userSlice.js";
 
 function SignIn() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+
   const handleChange = (e) => {
     const value = e.target.value;
     setFormData({ ...formData, [e.target.id]: value });
@@ -17,11 +24,10 @@ function SignIn() {
     e.preventDefault();
     // validate
     if (!formData.email || !formData.password) {
-      return setErrorMessage("All fields must be required !");
+      return dispatch(signInFailure("All required fields!"));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -32,16 +38,15 @@ function SignIn() {
       const data = await res.json();
 
       if (data.success === false) {
-        setErrorMessage(data.message);
-        setLoading(false);
+        return dispatch(signInFailure(data.message));
       }
       // success
       if (res.ok) {
         navigate("/");
+        return dispatch(signInSuccess(data));
       }
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     }
   };
   return (
