4e8a6b1050725775e8774324711fe3271e32f27e
LeHaiTho <letho11112002@gmail.com>
2024-06-14 12:28:02 +0700

    add redux persist (sign-in save data in localStorage)

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice.js";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+// lưu auth vào local storage nên dùng persist của thư viện redux-persist
+// khai báo rootReducer(dùng combineReducer)
+const rootReducer = combineReducers({
+  user: userReducer,
 });
+// cấu hình persistconfig
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+// persistReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// thay đổi store theo persistReducer và thêm middleware tránh lỗi của redux
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+// export persistor từ store
+export let persistor = persistStore(store);
